3ec2cda304086d194b550e797ee894aa48f09c6f
venkatest <venkatesh.tvl@gmail.com>
2024-07-03 15:50:37 -0400

    moduel3 final

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
       path(route='register', view=views.registration, name='register'),
     # path for dealer reviews view
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
-
+from .models import CarMake, CarModel
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -22,6 +22,17 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+    
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
