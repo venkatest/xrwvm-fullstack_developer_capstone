3189b4f462bbcd4e01e9341f1a971cb2e28dfa05
Venkatesh Sampath <so02wn@insim.biz>
2024-07-05 15:34:04 +0200

    updates

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
 
 
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('type', 'car_make', 'year')
+    search_fields = ('name', 'car_make__name')
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,24 +3,26 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
-    path(route='register', view=views.registration, name='register'),
-
-    # path for dealer reviews view
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-
-    # path for add a review view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='logout', view=views.logout_view, name='logout'),
+    path(route='register/', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='get_dealers',
+         view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details, name='get_dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>',
+         view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='get_inventory/<int:dealer_id>',
+         view=views.get_inventory, name='get_inventory'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
